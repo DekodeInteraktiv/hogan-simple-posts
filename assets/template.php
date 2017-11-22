0c981b63faa89d6e1af631f037743ea5cd7a8598
pederan <peder1976@gmail.com>
2017-11-22 15:16:28 +0100

    wp query testing

--- assets/template.php
+++ assets/template.php
@@ -16,20 +16,27 @@ if ( ! defined( 'ABSPATH' ) || ! ( $this instanceof Simple_Posts ) ) {
 foreach ( $this->list as $item ) : ?>
 	<li class="list-item">
 		<a href="<?php echo $item->url; ?>">
-			<?php if ( ! empty( $item->featured_image ) ) {
-				printf( '<p class="featured-image">%s</p>', $item->featured_image );
-			}
-			?>
-			<?php
-			if ( ! empty( $item->title ) ) {
-				printf( '<h2>%s</h2>', esc_html( $item->title ) );
-			}
-			?>
-			<?php
-			if ( ! empty( $item->title ) ) {
-				printf( '<p class="entry-summary">%s</p>', wp_kses_post( $item->excerpt ) );
-			}
-			?>
+
+			<?php if ( 'no_image' !== $this->card_look ) : ?>
+				<div class="column">
+					<?php if ( ! empty( $item->featured_image ) ) {
+						printf( '<p class="featured-image">%s</p>', $item->featured_image );
+					}
+					?>
+				</div>
+			<?php endif; ?>
+			<div class="column">
+				<?php
+				if ( ! empty( $item->title ) ) {
+					printf( '<h2>%s</h2>', esc_html( $item->title ) );
+				}
+				?>
+				<?php
+				if ( ! empty( $item->title ) ) {
+					printf( '<p class="entry-summary">%s</p>', wp_kses_post( $item->excerpt ) );
+				}
+				?>
+			</div>
 		</a>
 	</li>
 <?php endforeach;
--- class-simple-posts.php
+++ class-simple-posts.php
@@ -35,6 +35,13 @@ if ( ! class_exists( '\\Dekode\\Hogan\\Simple_Posts' ) && class_exists( '\\Dekod
 		public $list;
 
 		/**
+		 * Card look (Automatic or Manual).
+		 *
+		 * @var $card_look
+		 */
+		public $card_look;
+
+		/**
 		 * Module constructor.
 		 */
 		public function __construct() {
@@ -44,12 +51,6 @@ if ( ! class_exists( '\\Dekode\\Hogan\\Simple_Posts' ) && class_exists( '\\Dekod
 
 			add_filter( 'hogan/module/simple_posts/inner_wrapper_tag', function () {
 				return 'ul';
-			} );
-
-			add_filter( 'hogan/module/simple_posts/inner_wrapper_classes', function ( $classes ) {
-				$classes[] = 'list-items';
-
-				return $classes;
 			} );
 
 			parent::__construct();
@@ -117,6 +118,7 @@ if ( ! class_exists( '\\Dekode\\Hogan\\Simple_Posts' ) && class_exists( '\\Dekod
 					],
 					'post_type'         => [
 						0 => 'post',
+						1 => 'page',
 					],
 					'taxonomy'          => [],
 					'filters'           => [
@@ -132,11 +134,11 @@ if ( ! class_exists( '\\Dekode\\Hogan\\Simple_Posts' ) && class_exists( '\\Dekod
 					'return_format'     => 'id',
 				],
 				[
-					'type'              => 'select',
+					'type'              => 'taxonomy',
 					'key'               => $this->field_key . '_automatic_list',
 					'label'             => __( 'Automatic List', 'hogan-simple-posts' ),
 					'name'              => 'automatic_list',
-					'instructions'      => __( 'Choose post type', 'hogan-simple-posts' ),
+					'instructions'      => __( 'Choose category', 'hogan-simple-posts' ),
 					'required'          => 1,
 					'conditional_logic' => [
 						[
@@ -147,18 +149,9 @@ if ( ! class_exists( '\\Dekode\\Hogan\\Simple_Posts' ) && class_exists( '\\Dekod
 							],
 						],
 					],
-					'choices'           => [
-						'post' => __( 'Post', 'hogan-simple-posts' ),
-					],
-					'default_value'     => [
-						0 => 'post',
-					],
-					'allow_null'        => 0,
-					'multiple'          => 0,
-					'ui'                => 1,
-					'ajax'              => 0,
-					'return_format'     => 'value',
-					'placeholder'       => '',
+					'taxonomy'          => 'category',
+					'field_type'        => 'select',
+					'return_format'     => 'id',
 					'wrapper'           => [
 						'width' => '50',
 					],
@@ -198,10 +191,21 @@ if ( ! class_exists( '\\Dekode\\Hogan\\Simple_Posts' ) && class_exists( '\\Dekod
 		public function load_args_from_layout_content( $content ) {
 
 			$this->list_type = $content['list_type'];
+			$this->card_look = $content['card_look'];
 
 			if ( 'manual' === $this->list_type ) :
-				$this->list = $this->populate_list( $content['manual_list'] );
+				$this->list = $this->populate_manual_list( $content['manual_list'] );
+			elseif ( 'automatic' === $this->list_type ) :
+				$this->list = $this->populate_automatic_list( $content['automatic_list'], $content['number_of_items'] );
 			endif;
+
+			add_filter( 'hogan/module/simple_posts/inner_wrapper_classes', function ( $classes ) {
+				$classes[] = 'list-items';
+				$classes[] = 'card-look-' . $this->card_look;
+
+				return $classes;
+			} );
+
 
 			parent::load_args_from_layout_content( $content );
 		}
@@ -214,13 +218,68 @@ if ( ! class_exists( '\\Dekode\\Hogan\\Simple_Posts' ) && class_exists( '\\Dekod
 		}
 
 		/**
-		 * Create list of post objects.
+		 * Create list of post objects from post ids picked in manual list.
+		 *
+		 * @param array $category Category to fetch posts from
+		 * @param int $number_of_posts Number of posts to fetch
+		 *
+		 * @return array List of post items for the template
+		 */
+		protected function populate_automatic_list( $category, $number_of_posts ) {
+
+
+			/*$args = array(
+				'post_type' => 'post',
+				'posts_per_page' => $number_of_posts,
+				'tax_query' => array(
+					array(
+						'taxonomy' => 'category',
+						'field'    => 'term_id',
+						'terms'    => $category,
+					),
+				),
+			);*/
+
+			$args = [
+				'post_type' => [ 'post', 'page' ],
+				'orderby' => 'post__in',
+				'post__in'  => [ 126, 28, 1 ],
+			];
+
+			$query = new \WP_Query( $args );
+
+			$list = [];
+
+			if ( $query->have_posts() ) {
+				while ( $query->have_posts() ) {
+					$query->the_post();
+					// display content
+					$post_id = get_the_id();
+					$excerpt = empty( $query->post_excerpt ) ? wp_trim_words( get_the_content(), 20 ) : $query->post_excerpt;
+					$item    = [
+						'title'          => get_the_title( $post_id ),
+						'excerpt'        => $excerpt,
+						'featured_image' => get_the_post_thumbnail( $post_id ),
+						'url'            => get_the_permalink( $post_id ),
+					];
+
+					$list[] = (object) $item;
+				}
+			}
+
+			wp_reset_postdata();
+
+			return $list;
+		}
+
+		/**
+		 * Create list of post objects from post ids picked in manual list.
 		 *
 		 * @param array $post_ids List of ids.
 		 *
 		 * @return array List of post items for the template
 		 */
-		protected function populate_list( $post_ids ) {
+		protected function populate_manual_list( $post_ids ) {
 			$list = [];
 
 			foreach ( $post_ids as $post_id ) {
