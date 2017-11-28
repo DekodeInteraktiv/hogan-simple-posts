b70872523e98f4b26d08b350ebd8eee4e7264b39
pederan <peder1976@gmail.com>
2017-11-28 09:30:54 +0100

    renamed function hooks

--- hogan-simple-posts.php
+++ hogan-simple-posts.php
@@ -22,20 +22,20 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-add_action( 'plugins_loaded', __NAMESPACE__ . '\\hogan_text_load_textdomain' );
-add_action( 'hogan/include_modules', __NAMESPACE__ . '\\hogan_text_register_module' );
+add_action( 'plugins_loaded', __NAMESPACE__ . '\\hogan_load_textdomain' );
+add_action( 'hogan/include_modules', __NAMESPACE__ . '\\hogan_register_module' );
 
 /**
  * Register module text domain
  */
-function hogan_text_load_textdomain() {
+function hogan_load_textdomain() {
 	\load_plugin_textdomain( 'hogan-simple-posts', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
 }
 
 /**
  * Register module in Hogan
  */
-function hogan_text_register_module() {
+function hogan_register_module() {
 	require_once 'class-simple-posts.php';
 	\hogan_register_module( new \Dekode\Hogan\Simple_Posts() );
 }
