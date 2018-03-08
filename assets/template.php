892005178b07b6c4a5499d0ca02b04f1a944ac01
pederan <peder1976@gmail.com>
2018-03-08 18:07:17 +0100

    - h2->h3 as heading tag - p->div for featured image wrapper

--- assets/template.php
+++ assets/template.php
@@ -24,11 +24,11 @@ if ( $this->query->have_posts() ) : ?>
 				<a href="<?php echo esc_url( get_the_permalink() ); ?>">
 					<?php if ( true === apply_filters( 'hogan/module/simple_post/show_image_column', 'small' !== $this->card_type, $this ) ) : ?>
 						<div class="column">
-							<p class="featured-image"><?php echo get_the_post_thumbnail( null, apply_filters( 'hogan/module/simple_posts/image_size', 'post-thumbnail', $this ) ); ?></p>
+							<div class="featured-image"><?php echo get_the_post_thumbnail( null, apply_filters( 'hogan/module/simple_posts/image_size', 'post-thumbnail', $this ) ); ?></div>
 						</div>
 					<?php endif; ?>
 					<div class="column">
-						<h2 class="entry-title"><?php the_title_attribute(); ?></h2>
+						<h3 class="entry-title"><?php the_title_attribute(); ?></h3>
 						<?php the_excerpt(); ?>
 					</div>
 				</a>
